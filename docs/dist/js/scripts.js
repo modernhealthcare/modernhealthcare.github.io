67d98c6e6f323c71b58e47623748478f5a7f8f19
Christopher Dunn <hello@cdunn.io>
2015-01-13 15:45:36 -0600

    Added back to top link

--- docs/dist/js/scripts.js
+++ docs/dist/js/scripts.js
@@ -28,6 +28,18 @@ $(document).ready(function () {
     });
   });
 
+  var offset = 300,
+  offsetOpacity = 1200,
+  scrollTopDuration = 700,
+  $toTop = $('.to-top');
+
+  $(window).scroll(function() {
+    ( $(this).scrollTop() > offset ) ? $toTop.addClass('is-visible') : $toTop.removeClass('is-visible fade-out');
+    if ( $(this).scrollTop() > offsetOpacity ) {
+      $toTop.addClass('fade-out');
+    }
+  });
+
   var d = new Date();
   var n = d.getFullYear();
   $('.current-yr').text(n);
