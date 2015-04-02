9aa9f907f5c4dd6e41e81e13bfd715a6cc2199bd
Christopher Dunn <hello@cdunn.io>
2015-04-02 12:23:13 -0500

    Updated scripts

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,9 +1,11 @@
 $(document).ready(function () {
 
+  var $this = $(this);
+
   $('h1,h2,h3,h4,h5,h6').each(function() {
     var link = '<span class="icon-link"></span>';
-    if ($(this).attr('id')) {
-      $(this).prepend(link);
+    if ($this.attr('id')) {
+      $this.prepend(link);
     }
   });
 
@@ -14,37 +16,12 @@ $(document).ready(function () {
       }, 400);
       e.preventDefault();
     });
-    $('a[href*="#"]:not([href="#"])').click(function(e) {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        if (target.length) {
-          $('html,body').animate({
-          scrollTop: target.offset().top
-          }, 400);
-        }
-      }
-      e.preventDefault();
-    });
-  });
-
-  var offset = 300,
-  offsetOpacity = 1200,
-  scrollTopDuration = 700,
-  $toTop = $('.to-top');
-
-  $(window).scroll(function() {
-    ( $(this).scrollTop() > offset ) ? $toTop.addClass('is-visible') : $toTop.removeClass('is-visible fade-out');
-    if ( $(this).scrollTop() > offsetOpacity ) {
-      $toTop.addClass('fade-out');
-    }
   });
 
   var d = new Date();
   var n = d.getFullYear();
-  $('.current-yr').text(n);
-
-
+  var $copyright = $('[data-js="copyright"]');
+  $copyright.html('Copyright &copy; ' + n + ' <a href="http://www.modernhealthcare.com">Modern Healthcare</a>');
 });
 
 /* http://prismjs.com/download.html?themes=prism&languages=markup+css+clike+javascript+scss+bash+handlebars */
